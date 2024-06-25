b19cd0dcdac5440cb177a518f9f7f80cf31a0a68
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-06-25 23:28:39 +0500

    update: get devices query dto

--- src/device/dto/get-devices.dto.ts
+++ src/device/dto/get-devices.dto.ts
@@ -1,8 +1,33 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsArray, IsMongoId, IsOptional, IsString } from 'class-validator';
 import { PaginationQueryDto } from '../../common/dto/pagination.dto';
 
 export class GetDevicesQueryDto extends PaginationQueryDto {
   @IsOptional()
   @IsString()
   search?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  organization?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  site?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  building?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  floor?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  room?: string[];
 }
